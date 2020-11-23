abeb51d66b0639f6b5270e43265668184a17740f
ArcticRous <72030881+ArcticRous@users.noreply.github.com>
2020-11-23 11:50:58 -0600

    Update app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,12 +5,11 @@ import {Routes,RouterModule} from '@angular/router';
 
 // importar todos los componenetes que uilizaremos
 import { HomeComponent } from './home/home.component';
-import { GestionComponent } from './gestion/gestion.component';
+
 //import { VideojuegoComponent } from './videojuego/videojuego.componet';
 
 // ruta
 const appRoutes: Routes =[
-    {path: 'gestiones', component: GestionComponent},
     {path: '**', component: HomeComponent}
 
 ];
