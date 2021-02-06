bc8ac3a76d18cb435214c937f16149df135431a7
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-02-06 09:50:21 -0600

    Se agrego componente generador de reportes y se incrusto el código para extrar de daros de Analytics pero aun no se acomoda a Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CuentasComponent } from './components/cuentas/cuentas.component';
+import { GeneradorReportesComponent } from './components/generador-reportes/generador-reportes.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'home' , component: HomeComponent},
   {path: 'cuentas', component: CuentasComponent},
+  {path: 'generadorReportes', component: GeneradorReportesComponent},
   {path: '**' , redirectTo: 'login'}
 ];
 
---
+++ src/app/components/generador-reportes/generador-reportes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generador-reportes',
+  templateUrl: './generador-reportes.component.html',
+  styleUrls: ['./generador-reportes.component.css']
+})
+export class GeneradorReportesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
